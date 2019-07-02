e0569ffea26b779b0ee0be3a31174074b80793c0
csporto23 <Csporto23@gmail.com>
2019-07-02 14:25:43 -0500

    heroku db connect

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const handlebars = require("handlebars");
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
