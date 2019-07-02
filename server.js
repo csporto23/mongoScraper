13e05267df8359e8084ad744654f9cf3c3b9485f
csporto23 <Csporto23@gmail.com>
2019-07-02 14:37:07 -0500

    folder structure and initial server setup

--- server.js
+++ server.js
@@ -7,3 +7,35 @@ const handlebars = require("handlebars");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+const db = require("./models");
+
+const PORT = 8080;
+
+const app = express();
+
+
+//logs and request
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//public folder
+app.use(express.static("public"));
+
+
+
+
+
+
+
+//routes
+
+
+
+
+
+//server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
+});
