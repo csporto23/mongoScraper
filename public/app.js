c8503c934bbc4c6b818420a1427b219604089341
csporto23 <Csporto23@gmail.com>
2019-07-02 21:31:09 -0500

    app.js logic

---
+++ public/app.js
@@ -0,0 +1,16 @@
+$.getJSON("/articles", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        $("#articles").append("<p data id='" + data[i]._id + ">" + data[i].title + "<br />" + data[i].link + "</p>" + data[i].p + "</p>");
+    }
+});
+
+
+$(document).on("click", "p", function() {
+    $("#notes").empty();
+    const thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+})
--- server.js
+++ server.js
@@ -47,6 +47,9 @@ app.get("/scrape", function(req, res) {
         result.link = $(this)
           .children("a")
           .attr("href");
+          result.p = $(this)
+          .children("a")
+          .text();
   
         // Create a new Article using the `result` object built from scraping
         db.Article.create(result)
@@ -62,6 +65,41 @@ app.get("/scrape", function(req, res) {
   
       // Send a message to the client
       res.send("Scrape Complete");
+    });
+  });
+
+  app.get("/articles", function(req, res) {
+    db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle); 
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+  });
+
+  app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({_id: req.params.id})
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+  });
+
+
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
     });
   });
 
